718eb2341c2a35d4a3d1d9b689eeb23feeaaed4f
lenmon <lenmon@gmail.ccom>
2025-12-23 16:09:03 +0800

    添加测试发送方法

--- src/main/java/cn/keeponline/telegram/controller/TestController.java
+++ src/main/java/cn/keeponline/telegram/controller/TestController.java
@@ -1,14 +1,23 @@
 package cn.keeponline.telegram.controller;
 
+import cn.keeponline.telegram.dto.uuudto.UUUGroupMemberDTO;
 import cn.keeponline.telegram.entity.UserTask;
 import cn.keeponline.telegram.mapper.UserTaskMapper;
 import cn.keeponline.telegram.service.impl.TaskServiceImpl;
+import cn.keeponline.telegram.talktools.services.UuutalkApiClient;
+import cn.keeponline.telegram.talktools.ws.UUTalkWsCore;
+import cn.keeponline.telegram.talktools.ws.WebSocketWrapper;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.WebSocket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static cn.keeponline.telegram.service.impl.TaskServiceImpl.smetaMap;
@@ -57,6 +66,46 @@ public class TestController {
     public void getSmata() {
         log.info("smata数量: {}", smetaMap.size());
         log.info("smeta信息: {}", smetaMap);
+    }
+
+    @RequestMapping("/sendImageToGroupUser")
+    public String sendImageToGroupUser(@RequestParam("token") String token,
+                                       @RequestParam("uid") String uid, @RequestParam("groupId") String groupId) {
+        Map<String, WebSocketWrapper> uuuSocketMap = TaskServiceImpl.uuuSocketMap;
+        WebSocketWrapper webSocketWrapper = uuuSocketMap.get(token);
+        if (webSocketWrapper == null) {
+            return "ws 未连接";
+        }
+
+        UuutalkApiClient uuutalkApiClient = new UuutalkApiClient();
+        List<UUUGroupMemberDTO> uuuGroupMemberDTOS = new ArrayList<>();
+        try {
+            uuuGroupMemberDTOS = uuutalkApiClient.syncGroupMembers(token, groupId, 0, 100);
+        } catch (IOException e) {
+            log.error("", e);
+            return "拉取群成员失败";
+        }
+        if (CollectionUtils.isEmpty(uuuGroupMemberDTOS)) {
+            return "拉取群成员为空";
+        }
+
+        for (UUUGroupMemberDTO uuuGroupMemberDTO : uuuGroupMemberDTOS) {
+            try {
+                String to_uid = uuuGroupMemberDTO.getUid();
+                Integer role = uuuGroupMemberDTO.getRole();
+                if (to_uid.equals(uid) || role == 1) {
+                    continue;
+                }
+                UUTalkWsCore.sendPictureMessage(webSocketWrapper,
+                        "file/preview/chat/1/06e1321af60a42bf8ec6b15facfaaf3d/1a3ba617f3384eb8374255a27ecdb7b7.jpg",
+                        to_uid,
+                        1,
+                        uid);
+            } catch (Exception e) {
+                log.error("", e);
+            }
+        }
+        return "发送成功";
     }
 
 
---
+++ src/main/java/cn/keeponline/telegram/dto/uuudto/UUUGroupMemberDTO.java
@@ -0,0 +1,64 @@
+package cn.keeponline.telegram.dto.uuudto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UUUGroupMemberDTO {
+
+    private Long id;
+    private String uid;
+
+    @JsonProperty("group_no")
+    private String groupNo;
+
+    private String name;
+    private String remark;
+    private Integer role;
+    private Long version;
+
+    @JsonProperty("is_deleted")
+    private Integer isDeleted;
+
+    private Integer status;
+
+    @JsonProperty("is_destroy")
+    private Integer isDestroy;
+
+    private String vercode;
+
+    @JsonProperty("invite_uid")
+    private String inviteUid;
+
+    private Integer robot;
+
+    @JsonProperty("forbidden_expir_time")
+    private Long forbiddenExpirTime;
+
+    @JsonProperty("is_special_short_no")
+    private Integer isSpecialShortNo;
+
+    @JsonProperty("sign_list")
+    private List<Object> signList;
+
+    private Integer vip;
+
+    @JsonProperty("vip_expire")
+    private Long vipExpire;
+
+    private String avatar;
+
+    @JsonProperty("created_at")
+    private String createdAt;
+
+    @JsonProperty("updated_at")
+    private String updatedAt;
+
+    @JsonProperty("user_status")
+    private Integer userStatus;
+
+    @JsonProperty("setting_blacklist")
+    private Integer settingBlacklist;
+}
